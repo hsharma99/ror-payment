722c502e911847ef31aa3c12a0c2e8901a4d6261
Himanshu <himanshu.sharma@cignex.com>
2015-08-05 21:21:46 +0530

    Github push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '82c09d88ee7e2a5f8dbf7a0e790ddbe497c1b084fbc86597c1226cf0d3fe24aa22fa08014281671971c302bc7023cebfcecc80d121962a16df755dcf0b6a5f6c'
